a2dd485bad053f17a449a2140ffe833866a593a8
narbek <narbek.abdullayev@gmail.ru>
2022-03-12 21:37:18 +0600

    admin categories pagination

--- src/layouts/Admin/Categories/index.tsx
+++ src/layouts/Admin/Categories/index.tsx
@@ -1,17 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import LoadingButton from "../../../components/LoadingButton";
+import Pagination from "../../../components/Pagination";
 import * as S from "./style";
+import * as G from "../../../globalStyle";
+import { Link, useSearchParams } from "react-router-dom";
 
 interface CategoriesProps {
   children?: React.ReactNode;
 }
 
-const Categories = ({children} : CategoriesProps) => {
+const Categories = ({ children }: CategoriesProps) => {
+  const [data, setData] = useState<any>({});
+  let [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    axios
+      .get(
+        "http://comfort.loc/api/categories/list?limit=3&page=" +
+          searchParams.get("page")
+      )
+      .then(function (response) {
+        setData(response.data);
+      });
+  }, [searchParams]);
+
   return (
     <S.Categories>
-        <S.Inner>
-        
-        </S.Inner>
-      
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Parent category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.data?.map((category: any) => {
+            return (
+              <tr key={category.id}>
+                <td>{category.id}</td>
+                <td>{category.name}</td>
+                <td>Vend</td>
+                <td>
+                  <S.Actions>
+                    <LoadingButton isLoading={false} size="small" color="red">
+                      Delete
+                    </LoadingButton>
+                    <G.Button size="small" color="orange">
+                      Edit
+                    </G.Button>
+                  </S.Actions>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      {data.total && <Pagination totalItem={data.total} perPage={3} />}
     </S.Categories>
   );
 };
--- src/layouts/Admin/Categories/style.ts
+++ src/layouts/Admin/Categories/style.ts
@@ -1,12 +1,15 @@
 import styled from "styled-components";
+import { Button } from "../../../globalStyle";
 
 export const Categories = styled.div`
+	table {
+		margin-bottom: 30px;
+	}
+`;
 
-
-`
-
-export const Inner = styled.div`
-
-
-`
- 
+export const Actions = styled.div`
+	${Button} {
+		margin-right: 10px;
+	}
+`;
+export const Inner = styled.div``;
