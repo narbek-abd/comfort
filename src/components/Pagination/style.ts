fa1e3903f59f10cbfdecc042d9ff38cd86b1ef42
narbek <narbek.abdullayev@gmail.ru>
2022-03-13 21:27:10 +0600

    pagination fix

--- src/components/Pagination/style.ts
+++ src/components/Pagination/style.ts
@@ -4,12 +4,20 @@ export const Pagination = styled.ul`
 	display: flex;
 `;
 
-export const Item = styled.ul<{ active?: boolean }>`
-	margin: 0 3px;
+export const Item = styled.li<{ active?: boolean }>`
+	display: flex;
+	align-items: end;
 	a {
+		cursor: pointer;
+		border-radius: 3px;
+		margin: 0 3px;
 		color: #fff;
 		background-color: ${(props) =>
 			props.active ? "var(--color-pink)" : "#D06593A1"};
 		padding: 0.375rem 0.75rem;
+	}
+
+	span {
+		margin-bottom: 5px;
 	}
 `;
