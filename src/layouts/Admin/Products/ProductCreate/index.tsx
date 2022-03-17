cbf6ac1716f2db5376df54d06f7fde88551950f8
narbek <narbek.abdullayev@gmail.ru>
2022-03-17 21:44:41 +0600

    product create form

--- src/layouts/Admin/Products/ProductCreate/index.tsx
+++ src/layouts/Admin/Products/ProductCreate/index.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import * as yup from 'yup'; 
-import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 import * as G from "../../../../globalStyle";
 import * as S from "./style";
+import {
+  getCategoriesWithChildren,
+  getCategory,
+} from "../../../../api/Category";
 
 type FormFields = {
   name: string;
@@ -14,7 +18,11 @@ type FormFields = {
 
 const validationSchema = yup.object().shape({
   name: yup.string().required(),
-  price: yup.number().typeError('you must specify a number').required().integer(),
+  price: yup
+    .number()
+    .typeError("you must specify a number")
+    .required()
+    .integer(),
 });
 
 const ProductCreate = () => {
@@ -26,26 +34,79 @@ const ProductCreate = () => {
   } = useForm<FormFields>({ resolver: yupResolver(validationSchema) });
   const onSubmit: SubmitHandler<FormFields> = (data) => console.log(data);
 
+  const [categories, setCategories] = useState([]);
+  const [selectList, setSelectList] = useState([]);
+
+  useEffect(() => {
+    getCategoriesWithChildren().then((response) => {
+      setCategories(response.data);
+      setSelectList([{ level: 1, list: response.data }]);
+    });
+  }, []);
+
+  function handleCategoryChange(e: React.ChangeEvent) {
+    let selectedCategoryId = (e.target as HTMLInputElement).value;
+    let selectLevel = +(e.target as HTMLInputElement).closest("select").dataset
+      .level;
+
+    getCategory(+selectedCategoryId).then((response) => {
+      let selectedCategory = response.data;
+
+      if (selectedCategory === undefined) return;
+
+      let hasChildren = selectedCategory.children.length > 0;
+      if (!hasChildren) {
+        console.log("WOO");
+        return;
+      }
+
+      let filteredSelectList = selectList.filter(
+        (list) => list.level <= selectLevel
+      );
+
+      setSelectList([
+        ...filteredSelectList,
+        { level: ++selectLevel, list: selectedCategory.children },
+      ]);
+    });
+  }
+
   return (
     <S.Create>
       <form onSubmit={handleSubmit(onSubmit)}>
         <S.Group>
-          <input
-            type="text"
-            placeholder="name"
-            {...register("name")}
-          />
+          <input type="text" placeholder="name" {...register("name")} />
           {errors.name && <span>{errors.name.message}</span>}
         </S.Group>
 
         <S.Group>
-          <input
-            type="text"
-            placeholder="price"
-            {...register("price")}
-          />
+          <input type="text" placeholder="price" {...register("price")} />
           {errors.price && <span>{errors.price.message}</span>}
         </S.Group>
+
+        {selectList.map((list) => {
+          return (
+            <S.Group key={list.level}>
+              <select
+                name="category_id"
+                onChange={handleCategoryChange}
+                data-level={list.level}
+                defaultValue=""
+              >
+                <option value="" disabled>
+                  Select category
+                </option>
+                {list.list.map((category: any) => {
+                  return (
+                    <option key={category.id} value={category.id}>
+                      {category.name}
+                    </option>
+                  );
+                })}
+              </select>
+            </S.Group>
+          );
+        })}
 
         <S.Group>
           <G.Button type="submit">submit</G.Button>
