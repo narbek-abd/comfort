eb1553e83347a6dcc1615b9c4987b2b5b6e98dc6
narbek <narbek.abdullayev@gmail.ru>
2022-03-20 16:31:22 +0600

    product create form finshed

--- src/layouts/Admin/Products/ProductCreate/index.tsx
+++ src/layouts/Admin/Products/ProductCreate/index.tsx
@@ -1,115 +1,139 @@
 import React, { useState, useEffect } from "react";
+
 import { useForm, SubmitHandler } from "react-hook-form";
-import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { ProductValidation } from "../../../../validation/Product";
 
-import * as G from "../../../../globalStyle";
+import { ProductFormTypes } from "../../../../types/ProductTypes";
+import { CategoryTypes } from "../../../../types/CategoryTypes";
+import { UploadedImageTypes } from "../../../../types/UploadedImageTypes";
+
 import * as S from "./style";
-import {
-  getCategoriesWithChildren,
-  getCategory,
-} from "../../../../api/Category";
+import * as G from "../../../../globalStyle";
 
-type FormFields = {
-  name: string;
-  parend_id: number;
-  price: number;
-};
+import { getCategoriesWithChildren } from "../../../../api/Category";
+import { createProduct } from "../../../../api/Product";
 
-const validationSchema = yup.object().shape({
-  name: yup.string().required(),
-  price: yup
-    .number()
-    .typeError("you must specify a number")
-    .required()
-    .integer(),
-});
+import ImageUpload from "../../../../components/ImageUpload";
+import SelectNested from "../../../../components/SelectNested";
+import Alert from "../../../../components/Alert";
+import LoadingButton from "../../../../components/LoadingButton";
 
 const ProductCreate = () => {
   const {
     register,
     handleSubmit,
+    setError,
+    reset,
     formState: { errors },
-    getValues,
-  } = useForm<FormFields>({ resolver: yupResolver(validationSchema) });
-  const onSubmit: SubmitHandler<FormFields> = (data) => console.log(data);
+  } = useForm<ProductFormTypes>({ resolver: yupResolver(ProductValidation) });
 
+  /** Категорий */
   const [categories, setCategories] = useState([]);
-  const [selectList, setSelectList] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(0);
+  const [keyForReRender, setkeyForReRender] = useState(0);
 
   useEffect(() => {
     getCategoriesWithChildren().then((response) => {
       setCategories(response.data);
-      setSelectList([{ level: 1, list: response.data }]);
     });
   }, []);
 
-  function handleCategoryChange(e: React.ChangeEvent) {
-    let selectedCategoryId = (e.target as HTMLInputElement).value;
-    let selectLevel = +(e.target as HTMLInputElement).closest("select").dataset
-      .level;
-
-    getCategory(+selectedCategoryId).then((response) => {
-      let selectedCategory = response.data;
-
-      if (selectedCategory === undefined) return;
-
-      let hasChildren = selectedCategory.children.length > 0;
-      if (!hasChildren) {
-        console.log("WOO");
-        return;
-      }
-
-      let filteredSelectList = selectList.filter(
-        (list) => list.level <= selectLevel
-      );
-
-      setSelectList([
-        ...filteredSelectList,
-        { level: ++selectLevel, list: selectedCategory.children },
-      ]);
-    });
+  function onCategorySelectedWithNoChildren(category: CategoryTypes) {
+    setSelectedCategory(category.id);
   }
+
+  /** Изображение */
+  const [productImages, setProductImages] = useState([]);
+
+  function onUploadedImg(images: UploadedImageTypes[]) {
+    setProductImages(images);
+  }
+
+  /** UI */
+  const [alertmessage, setAlertMessage] = useState("");
+  const [alertvariant, setAlertvariant] = useState("success");
+  const [isLoading, setIsLoading] = useState(false);
+
+  /** Submit */
+  const onSubmit: SubmitHandler<ProductFormTypes> = (data) => {
+    if (selectedCategory === 0) {
+      setError("category_id", {
+        message: "Select category",
+      });
+
+      return;
+    }
+
+    if (productImages.length === 0) {
+      setError("images", {
+        message: "Upload product images",
+      });
+
+      return;
+    }
+
+    setIsLoading(true);
+
+    const formData = new FormData();
+    productImages.forEach((imgItem) => {
+      formData.append("images[]", imgItem.img);
+    });
+    formData.append("name", String(data.name));
+    formData.append("price", String(data.price));
+    formData.append("category_id", String(selectedCategory));
+
+    createProduct(formData).then((response) => {
+      setIsLoading(false);
+
+      if (response.status === 201) {
+        reset();
+        setAlertMessage("Category was created successfully");
+        setSelectedCategory(0);
+        setkeyForReRender((old) => ++old);
+      }
+    });
+  };
 
   return (
     <S.Create>
+      <Alert message={alertmessage} variant={alertvariant} />
+
       <form onSubmit={handleSubmit(onSubmit)}>
         <S.Group>
           <input type="text" placeholder="name" {...register("name")} />
-          {errors.name && <span>{errors.name.message}</span>}
+          {errors.name && <G.Err>{errors.name.message}</G.Err>}
         </S.Group>
 
         <S.Group>
           <input type="text" placeholder="price" {...register("price")} />
-          {errors.price && <span>{errors.price.message}</span>}
+          {errors.price && <G.Err>{errors.price.message}</G.Err>}
         </S.Group>
 
-        {selectList.map((list) => {
-          return (
-            <S.Group key={list.level}>
-              <select
-                name="category_id"
-                onChange={handleCategoryChange}
-                data-level={list.level}
-                defaultValue=""
-              >
-                <option value="" disabled>
-                  Select category
-                </option>
-                {list.list.map((category: any) => {
-                  return (
-                    <option key={category.id} value={category.id}>
-                      {category.name}
-                    </option>
-                  );
-                })}
-              </select>
-            </S.Group>
-          );
-        })}
+        <S.Group>
+          <S.SelectGroup>
+            <SelectNested
+              list={categories}
+              name="category_id"
+              desc="Select category"
+              onSelected={onCategorySelectedWithNoChildren}
+              keyForReRender={keyForReRender}
+            />
+          </S.SelectGroup>
+          {errors.category_id && <G.Err>{errors.category_id.message}</G.Err>}
+        </S.Group>
 
         <S.Group>
-          <G.Button type="submit">submit</G.Button>
+          <p style={{ marginBottom: "15px" }}>Select product images</p>
+          <ImageUpload
+            onUpload={onUploadedImg}
+            keyForReRender={keyForReRender}
+          />
+          {errors.images && <G.Err>{errors.images.message}</G.Err>}
+        </S.Group>
+
+        <S.Group>
+          <LoadingButton isLoading={isLoading}>Submit</LoadingButton>
         </S.Group>
       </form>
     </S.Create>
