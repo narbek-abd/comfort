4fa161baf5066c6ce04fba91104fc1c66a97bf4e
narbek <narbek.abdullayev@gmail.ru>
2022-05-29 11:23:10 +0600

    comments test

---
+++ src/layouts/Comments/CommentsForm/index.test.tsx
@@ -0,0 +1,27 @@
+import { screen } from "@testing-library/react";
+import { render } from "../../../test/test-utils";
+import CommentsForm from "./index";
+
+describe("CommentsForm component", () => {
+
+	test("should display the comments form", () => {
+		render(<CommentsForm product_id={99} onSubmit={() => {}} />);
+
+		expect(screen.getByPlaceholderText(/name/i)).toBeInTheDocument();
+		expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument();
+		expect(screen.getByPlaceholderText(/message/i)).toBeInTheDocument();
+		expect(screen.getByText(/submit/i)).toBeInTheDocument();
+	});
+
+	test("should display the editing text", () => {
+		render(
+			<CommentsForm
+				product_id={99}
+				onSubmit={() => {}}
+				editingText="wow"
+			/>
+		);
+
+		expect(screen.getByDisplayValue(/wow/i)).toBeInTheDocument();
+	});
+});
---
+++ src/layouts/Comments/index.test.tsx
@@ -0,0 +1,54 @@
+import { screen } from "@testing-library/react";
+import { render, store } from "../../test/test-utils";
+import "../../test/mock";
+import Comments from "./index";
+import axios from "axios";
+import { setUser } from "../../store/action-creators/User";
+
+describe("Comments component", () => {
+	beforeEach(() => {
+		axios.get.mockImplementation(() => {
+			return Promise.resolve({
+				data: {
+					data: [
+						{
+							id: 1,
+							user_id: 1,
+							name: "Alan",
+							created_at: "20.02.2022",
+							text: "Hello",
+							replies: [
+								{
+									id: 2,
+									user_id: 2,
+									name: "John",
+									created_at: "20.02.2022",
+									text: "Hi",
+								},
+							],
+						},
+					],
+					next_page_url: null,
+				},
+			});
+		});
+	});
+
+	test("should display comments", async () => {
+		render(<Comments product_id={99} />);
+
+		expect(await screen.findByText("Alan")).toBeInTheDocument();
+		expect(await screen.findByText("John")).toBeInTheDocument();
+		expect(await screen.findByText("Hello")).toBeInTheDocument();
+		expect(await screen.findByText("Hi")).toBeInTheDocument();
+	});
+
+	test("should display edit and delete buttons for the comment author", async () => {
+		render(<Comments product_id={99} />);
+
+		store.dispatch(setUser({ id: 1 }));
+
+		expect(await screen.findByText(/edit/i)).toBeInTheDocument();
+		expect(await screen.findByText(/delete/i)).toBeInTheDocument();
+	});
+});
