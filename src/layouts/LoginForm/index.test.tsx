9db97e8a99812609db83bf26b9162a6ff0a2d91c
narbek <narbek.abdullayev@gmail.ru>
2022-05-31 19:26:06 +0600

    login test

---
+++ src/layouts/LoginForm/index.test.tsx
@@ -0,0 +1,57 @@
+import { screen, waitFor } from "@testing-library/react";
+import { render, store } from "../../test/test-utils";
+import userEvent from "@testing-library/user-event";
+import LoginForm from "./index";
+import axios from "axios";
+
+describe("LoginForm component", () => {
+	beforeAll(() => {});
+
+	beforeEach(() => {
+		axios.post.mockImplementation(() =>
+			Promise.resolve({
+				data: {
+					user: {
+						id: 1,
+						name: "Alan",
+					},
+					token: "qwerty",
+				},
+			})
+		);
+	});
+
+	test("should add the user's data to the store when loging in", async () => {
+		render(<LoginForm />);
+
+		await userEvent.type(
+			screen.getByPlaceholderText(/email/i),
+			"alan@mail.ru"
+		);
+		await userEvent.type(
+			screen.getByPlaceholderText(/password/i),
+			"wow123"
+		);
+
+		await userEvent.click(screen.getByText(/submit/i));
+
+		await waitFor(() => {
+			expect(store.getState().user.data.id).toBe(1);
+		});
+	});
+
+	test("should validate the form", async () => {
+		render(<LoginForm />);
+
+		await userEvent.type(screen.getByPlaceholderText(/email/i), "blabla");
+
+		await userEvent.click(screen.getByText(/submit/i));
+
+		expect(
+			await screen.findByText("password is a required field")
+		).toBeInTheDocument();
+		expect(
+			await screen.findByText("email must be a valid email")
+		).toBeInTheDocument();
+	});
+});
