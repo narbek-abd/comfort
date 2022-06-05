ab1d5438329a52c3ad4e83d57f3ef0652223601c
narbek <narbek.abdullayev@gmail.ru>
2022-06-05 12:11:55 +0600

    test utils types

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, RenderOptions } from "@testing-library/react";
 import { createMemoryHistory } from "history";
 import { Router } from "react-router-dom";
 import { store } from "../store";
@@ -7,7 +7,7 @@ import { Provider } from "react-redux";
 
 let history;
 
-const AllTheProviders = ({ children }) => {
+const AllTheProviders = ({ children }: { children: React.ReactElement }) => {
   history = createMemoryHistory();
 
   return (
@@ -19,8 +19,10 @@ const AllTheProviders = ({ children }) => {
   );
 };
 
-const appRender = (ui, options) =>
-  render(ui, { wrapper: AllTheProviders, ...options });
+const appRender = (
+  ui: React.ReactElement,
+  options?: Omit<RenderOptions, "queries">
+) => render(ui, { wrapper: AllTheProviders, ...options });
 
 export * from "@testing-library/react";
 
