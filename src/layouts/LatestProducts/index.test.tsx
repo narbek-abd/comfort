8796f4f0148a92e89f99f38ef284845096843775
narbek <narbek.abdullayev@gmail.ru>
2022-06-06 13:16:12 +0600

    test latestProducts

---
+++ src/layouts/LatestProducts/index.test.tsx
@@ -0,0 +1,35 @@
+import { screen } from "@testing-library/react";
+import {render} from "../../test/test-utils";
+import LatestProducts from "./index";
+import axios from "axios";
+
+describe("LatestProducts component", () => {
+	beforeEach(() => {
+		(axios.get as jest.Mock).mockImplementation(() => {
+			return Promise.resolve({
+				data: {
+					data: [
+						{
+							id: 1,
+							name: "Iphone",
+							price: 123,
+							quantity: 12,
+							category: {
+								name: "phones",
+							},
+							images: [{ id: 1, image: "https://image.png" }],
+						},
+					],
+				},
+				status: 200,
+			});
+		});
+	});
+
+	test("should display component", async () => {
+		const { container } = render(<LatestProducts />);
+
+		expect(await screen.findByText("Iphone")).toBeInTheDocument();
+		expect(container).toMatchSnapshot();
+	});
+});
